c6696fb1d9e3eb09b7b81eb7d639abcb3c73c38f
jotanarciso <joaoznarciso@gmail.com>
2023-05-25 16:26:28 -0300

    feat:  use chalk to color output/erros

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
+import { Command } from "commander";
+import chalk from "chalk";
+
 const program = new Command();
 
-program.version('1.0.0');
-
-program
-    .command('command <arg>')
-    .description('description')
+const run = () => {
+  program.version("1.0.0");
+  program
+    .command("command <arg>")
+    .description("description")
     .action((arg) => {
-        console.log(arg);
+      console.log(arg);
     });
 
-program.parse(process.argv);
+  program.parse(process.argv);
+};
+
+try {
+  run();
+} catch (err) {
+  console.error(chalk.red("An error occurred:"), err);
+}
