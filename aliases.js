7f48c79905efedc8c5d621c5580c5de29f8d81e2
jotanarciso <joaoznarciso@gmail.com>
2023-05-26 12:54:29 -0300

    feat: new alias

--- aliases.js
+++ aliases.js
@@ -2,4 +2,7 @@ export default [
   ['#root', './'],
   ['#lib', './lib/'],
   ['#errors', './lib/errors/base.error.js'],
+  ['#utils', './lib/utils/'],
+  ['#polyfills', './lib/polyfills/'],
+  ['#targets', './lib/targets/'],
 ];
