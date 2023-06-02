6da88c5dd73269cdda422f2e13c9b6d6be5b0561
Magnun A V F <magnun.furtado@azion.com>
2023-06-02 18:08:41 -0300

    refactor: update simple-js example

---
+++ examples/simple-js-esm/index.js
@@ -0,0 +1,12 @@
+import messages from './messages.js';
+
+const message = messages[Math.floor(Math.random() * messages.length)];
+
+console.log('selected message:', message);
+
+return new Response(message, {
+  headers: new Headers([
+    ["X-Custom-Feat", "my random message"],
+  ]),
+  status: 200,
+});
