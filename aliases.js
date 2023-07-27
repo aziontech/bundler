31e8bf1c836a41736629e58ba63d8fa218c48e5b
João Narciso <joaoznarciso@gmail.com>
2023-07-27 16:32:11 -0300

    Release 1.1.0 (#75)

--- aliases.js
+++ aliases.js
@@ -2,7 +2,7 @@ export default [
   ['#root/*', './'],
   ['#lib/*', './lib'],
   ['#utils', './lib/utils/index.js'],
-  ['#polyfills', './lib/build/polyfills/index.js'],
+  ['#polyfills', './lib/polyfills/index.js'],
   ['#build', './lib/build/dispatcher/index.js'],
   ['#bundlers', './lib/build/bundlers/index.js'],
   ['#notations/*', './lib/notations'],
---
+++ examples/simple-ts-esm/main.ts
@@ -0,0 +1,24 @@
+/* eslint-disable */
+declare const AZION_VERSION_ID: string;
+
+import getMessages from "./messages.js";
+
+function getRandomMessage(messages: string[]): string {
+  const randomIndex: number = Math.floor(Math.random() * messages.length);
+
+  return messages[randomIndex];
+}
+
+function main(event: any) {
+  const messages = getMessages();
+  const message = getRandomMessage(messages);
+
+  console.log("selected message:", message);
+  console.log("VERSION_ID =", AZION_VERSION_ID);
+
+  return new Response(message, {
+    headers: new Headers([["X-Custom-Feat", "my random message"]]),
+    status: 200,
+  });
+}
+/* eslint-enable */
