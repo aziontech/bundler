168d9e9ff6f266f449e017e85a00b19f1ff0ca75
João Narciso <joaoznarciso@gmail.com>
2023-08-25 15:46:27 +0200

    feat: 'init' command + 'presets <create>' command (#87)
    * [ISSUE-62] feat: Windows Compatibility (#61)
    
    * chore: yml identation
    
    * chore: remove dev dependency
    
    * chore(deps): removing open dependency from package.json
    
    * fix(compatibility): read preset list
    
    * fix(compatibility): build process
    
    * fix(compatibility) Deployment and authentication process for windows users
    
    ---------
    
    Co-authored-by: João Narciso <joaoznarciso@gmail.com>
    Co-authored-by: Jose Carlos Batista <jc.bsfilho@gmail.com>
    
    * [ISSUE-18] Add esbuild and custom build config support  (#66)
    
    * [ISSUE-25] feat: local environment (server/runtime)  (#70)
    
    * feat: local environment
    
    * fix(build): update mode from 'server' to 'compute'
    
    * refactor(messages): update server running message
    
    * refactor: update 'statics' to 'storage' concept
    
    * style: exec identation
    
    * refactor(logs): improve real-time log feedback
    
    * feat: enable run/logs without DEBUG=true
    
    * feat(feedback): add 'next' scope for improved feedback
    
    * feat(feedback): add server/runtime scopes
    
    * refactor(server): improve code readability + open in browser
    
    * refactor(runtime): improved module for sandboxed/local worker execution
    
    * fix(feedback): typo
    
    * feat(edge): debugRequest interface
    
    * fix(build): mode typo
    
    * refactor(runtime): use @edge-runtime for local env
    
    * fix: Set correct angular build config
    
    * fix: Set correct build command in hexo prebuild
    
    * fix: Set correct error message in preset validation
    
    * fix: enable webpack minify and remove debug request
    
    ---------
    
    Co-authored-by: Magnun A V F <magnun.furtado@azion.com>
    
    * feat: Add nodejs polyfills (#71)
    
    Experimental feat.
    
    * fix: remove debug + enable minify
    
    * feat(preset): vue with vite
    
    * fix: Set correct trigger to release actions (#74)
    
    * chore: bump version
    
    * feat: enable 'eval'in the local env (#78)
    
    * fix: deploy statics path
    
    * fix(preset): vue with vite build
    
    * fix: '--version' command
    
    * refactor: vulcan run -> vulcan dev
    
    * feat: init command (templates) (#79)
    
    * feat: exec interactive mode
    
    * refactor: variable name
    
    * feat: global messages
    
    * feat: init template from frameworks
    
    * refactor: handle exceptions + improve feedback + improve vulcan global context (#81)
    
    * feat: exec interactive mode
    
    * refactor: variable name
    
    * feat: global messages
    
    * feat: init template from frameworks
    
    * refactor: improve readable code
    
    * fix: jsdoc line
    
    * refactor: jsdoc + vite/vue key
    
    * refactor: mv runtime polyfills to env
    
    * fix(preset): use relative packagemanger hexo
    
    * refactor: build error handler
    
    * fix: jsdoc line
    
    * refactor: improve vulcan global context
    
    * feat: read build entry exception handler
    
    * fix: starting build feedback scope
    
    * feat: feedback js/ts default entrypoint
    
    * feat: 'presets create' command
    
    * docs: update presets.md
    
    * docs: update overview.md
    
    * docs: update README.md
    
    * bump version
    
    ---------
    
    Co-authored-by: Rafael Benck <62527699+Rafaelbenck@users.noreply.github.com>
    Co-authored-by: Jose Carlos Batista <jc.bsfilho@gmail.com>
    Co-authored-by: Magnun A V F <103958633+MagnunAVFAzion@users.noreply.github.com>
    Co-authored-by: Magnun A V F <magnun.furtado@azion.com>

--- aliases.js
+++ aliases.js
@@ -2,7 +2,7 @@ export default [
   ['#root/*', './'],
   ['#lib/*', './lib'],
   ['#utils', './lib/utils/index.js'],
-  ['#polyfills', './lib/polyfills/index.js'],
+  ['#polyfills', './lib/env/polyfills/index.js'],
   ['#build', './lib/build/dispatcher/index.js'],
   ['#bundlers', './lib/build/bundlers/index.js'],
   ['#notations/*', './lib/notations'],
