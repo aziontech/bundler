a4c306a9ae63593a991e95ea5adb804c5e2121f6
Magnun A V F <magnun.furtado@azion.com>
2023-10-31 17:27:09 -0300

    chore: Add script to setup e2e test env

---
+++ tests/scripts/setup-e2e-env.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# color codes
+RED=31
+GREEN=32
+YELLOW=33
+BLUE=34
+MAGENTA=35
+CYAN=36
+DEFAULT=0
+
+log_with_color() {
+    local message="$1"
+    local color_code="$2"
+    echo -e "\e[${color_code}m${message}\e[0m"
+}
+
+#  isolate examples
+log_with_color "* STARTING ..." $GREEN
+
+SENTINEL_FILE="/vulcan/.initialized.keep"
+
+echo "MY_SENTINEL FILE: ${SENTINEL_FILE}"
+
+# TODO: improve this init check
+if test -f $SENTINEL_FILE; then
+    log_with_color "Container already initialized!" $CYAN
+else
+    log_with_color "Container NOT initialized! Initializing container ..." $YELLOW
+
+    # log_with_color "* Isolate examples" $GREEN
+    cp -r /vulcan/examples/ /examples/
+
+    # install vulcan locally
+    log_with_color "* Install vulcan" $GREEN
+    cd vulcan
+    yarn
+
+    # login in verdaccio registry
+    log_with_color "* Login in verdaccio" $GREEN
+    npx --yes npm-cli-login -u test -p 1234 -e test@domain.test -r http://verdaccio:4873
+
+    # publish vulcan in verdaccio
+    log_with_color "* Publish vulcan in verdaccio" $GREEN
+    npm publish --registry http://verdaccio:4873
+    npm info edge-functions --json --registry http://verdaccio:4873
+    npx --yes edge-functions@latest --help http://verdaccio:4873
+
+    cd /
+
+    echo "OK" > "$SENTINEL_FILE"
+
+    log_with_color "* DONE!" $GREEN
+fi
+
+# keep the container running
+tail -f /dev/null
