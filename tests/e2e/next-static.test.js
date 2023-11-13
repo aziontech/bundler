09fec8be655127674b83e18580740e53fc5a6326
Magnun A V F <magnun.furtado@azion.com>
2023-11-13 09:23:21 -0300

    test: Add next-static project e2e tests

---
+++ tests/e2e/next-static.test.js
@@ -0,0 +1,74 @@
+/* eslint-disable jest/expect-expect */
+import supertest from 'supertest';
+import puppeteer from 'puppeteer';
+import projectInitializer from '../utils/project-initializer.js';
+import projectStop from '../utils/project-stop.js';
+
+// timeout in minutes
+const TIMEOUT = 10 * 60 * 1000;
+
+const SERVER_PORT = 3005;
+const LOCALHOST_BASE_URL = `http://localhost:${SERVER_PORT}`;
+const EXAMPLE_PATH = '/examples/next/next-static';
+
+describe('E2E - next-static project', () => {
+  let request;
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    request = supertest(LOCALHOST_BASE_URL);
+
+    await projectInitializer(EXAMPLE_PATH, 'next', 'deliver', SERVER_PORT);
+
+    browser = await puppeteer.launch({ headless: 'new' });
+    page = await browser.newPage();
+  }, TIMEOUT);
+
+  afterAll(async () => {
+    await projectStop(SERVER_PORT, EXAMPLE_PATH.replace('/examples/', ''));
+
+    await browser.close();
+  }, TIMEOUT);
+
+  test('Should render home page in "/" route', async () => {
+    await page.goto(`${LOCALHOST_BASE_URL}/`);
+
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain('Get started by editing');
+    expect(pageContent).toContain(
+      'Find in-depth information about Next.js features and API.',
+    );
+    expect(pageTitle).toBe('Create Next App');
+  });
+
+  test('Should render a post page in "/blog/post-2/" route', async () => {
+    await page.goto(`${LOCALHOST_BASE_URL}/blog/post-2/`);
+
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain('My Post slug:');
+    expect(pageContent).toContain('post-2');
+    expect(pageTitle).toBe('Create Next App');
+  });
+
+  test('Should render misty mountains moria page in "/misty-mountains/moria" route', async () => {
+    await page.goto(`${LOCALHOST_BASE_URL}/misty-mountains/moria`);
+
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain('you shall not pass');
+    expect(pageTitle).toBe('Create Next App');
+  });
+
+  test('Should return correct asset', async () => {
+    await request
+      .get('/favicon.ico')
+      .expect(200)
+      .expect('Content-Type', /image/);
+  });
+});
