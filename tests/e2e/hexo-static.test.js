bd2c13d3350b5dd45b015176d986904dc9578463
Magnun A V F <magnun.furtado@azion.com>
2023-11-16 16:05:51 -0300

    test: Fix hexo e2e tests - use dynamic date

--- tests/e2e/hexo-static.test.js
+++ tests/e2e/hexo-static.test.js
@@ -11,6 +11,13 @@ const SERVER_PORT = 3009;
 const LOCALHOST_BASE_URL = `http://localhost:${SERVER_PORT}`;
 const EXAMPLE_PATH = '/examples/hexo-static';
 
+const now = new Date();
+const currentYear = now.getFullYear();
+const currentMonth = String(now.getMonth() + 1).padStart(2, '0');
+const currentDay = String(now.getDate()).padStart(2, '0');
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const dateWithSlashes = currentDate.replaceAll('-', '/');
+
 describe('E2E - hexo-static project', () => {
   let request;
   let browser;
@@ -39,7 +46,7 @@ describe('E2E - hexo-static project', () => {
 
     expect(pageTitle).toBe('Hexo');
     expect(pageContent).toContain('Other page to test.');
-    expect(pageContent).toContain('2023-11-14');
+    expect(pageContent).toContain(currentDate);
     expect(pageContent).toContain('This is your very first post');
   });
 
@@ -51,34 +58,34 @@ describe('E2E - hexo-static project', () => {
 
     expect(pageTitle).toBe('Archives | Hexo');
     expect(pageContent).toContain(
-      '<a href="/archives/2023" class="archive-year">2023</a>',
+      `<a href="/archives/${currentYear}" class="archive-year">${currentYear}</a>`,
     );
     expect(pageContent).toContain('<div class="archives">');
     expect(pageContent).toContain('Other page');
     expect(pageContent).toContain('Hello World');
   });
 
-  test('Should render "Hello World" post page in "/2023/11/14/hello-world/" route', async () => {
-    await page.goto(`${LOCALHOST_BASE_URL}/2023/11/14/hello-world/`);
+  test(`Should render "Hello World" post page in "/${dateWithSlashes}/hello-world/" route`, async () => {
+    await page.goto(`${LOCALHOST_BASE_URL}/${dateWithSlashes}/hello-world/`);
 
     const pageContent = await page.content();
     const pageTitle = await page.title();
 
     expect(pageTitle).toBe('Hello World | Hexo');
     expect(pageContent).not.toContain('Other page to test.');
-    expect(pageContent).toContain('2023-11-14');
+    expect(pageContent).toContain(currentDate);
     expect(pageContent).toContain('This is your very first post');
   });
 
-  test('Should render "Other page" post page in "/2023/11/14/hello-world/" route', async () => {
-    await page.goto(`${LOCALHOST_BASE_URL}/2023/11/14/other-page/`);
+  test(`Should render "Other page" post page in "/${dateWithSlashes}/hello-world/" route`, async () => {
+    await page.goto(`${LOCALHOST_BASE_URL}/${dateWithSlashes}/other-page/`);
 
     const pageContent = await page.content();
     const pageTitle = await page.title();
 
     expect(pageTitle).toBe('Other page | Hexo');
     expect(pageContent).toContain('Other page to test.');
-    expect(pageContent).toContain('2023-11-14');
+    expect(pageContent).toContain(currentDate);
     expect(pageContent).not.toContain('This is your very first post');
   });
 
