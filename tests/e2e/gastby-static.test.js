f23ffc95e12180029d71e85c493876806427eda8
Hendrik Soares <hendrik.soares@gmail.com>
2024-02-14 11:56:00 -0300

    fix: change to mountSSG and add unit test and sample to gastby

---
+++ tests/e2e/gastby-static.test.js
@@ -0,0 +1,67 @@
+/* eslint-disable jest/expect-expect */
+import supertest from 'supertest';
+import puppeteer from 'puppeteer';
+import projectInitializer from '../utils/project-initializer.js';
+import projectStop from '../utils/project-stop.js';
+import { getContainerPort } from '../utils/docker-env-actions.js';
+
+// timeout in minutes
+const TIMEOUT = 10 * 60 * 1000;
+
+let serverPort;
+let localhostBaseUrl;
+const EXAMPLE_PATH = '/examples/gastby-static';
+
+describe('E2E - gastby-static project', () => {
+  let request;
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    serverPort = getContainerPort();
+    localhostBaseUrl = `http://0.0.0.0:${serverPort}`;
+
+    request = supertest(localhostBaseUrl);
+
+    await projectInitializer(EXAMPLE_PATH, 'gastby', 'deliver', serverPort);
+
+    browser = await puppeteer.launch({ headless: 'new' });
+    page = await browser.newPage();
+  }, TIMEOUT);
+
+  afterAll(async () => {
+    await projectStop(serverPort, EXAMPLE_PATH.replace('/examples/', ''));
+
+    await browser.close();
+  }, TIMEOUT);
+
+  test('Should render home page in "/" route', async () => {
+    await page.goto(`${localhostBaseUrl}/`);
+
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain(
+      'Written by Kyle Mathews who lives and works in San Francisco building useful things.',
+    );
+    expect(pageContent).toContain(
+      'This is a custom description for SEO and Open Graph purposes, rather than the default generated excerpt. Simply add a description field to the frontmatter.',
+    );
+    expect(pageTitle).toBe('Gatsby Starter Blog');
+  });
+
+  test('Should render edge page in "/edge" route', async () => {
+    await page.goto(`${localhostBaseUrl}/edge`);
+
+    const pageContent = await page.content();
+
+    expect(pageContent).toContain('Running in Edge.');
+  });
+
+  test('Should return correct asset', async () => {
+    await request
+      .get('/favicon.ico')
+      .expect(200)
+      .expect('Content-Type', /image\/ico/);
+  });
+});
