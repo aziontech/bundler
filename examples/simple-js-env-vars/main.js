79d29c30c0fa02917f8324a1b90a801de0aec2b3
José Filho <jc.bsfilho@gmail.com>
2024-02-16 11:13:58 -0300

    chore(examples): adding the example of using env vars
    Example of using Azion Env Vars on Edge.

---
+++ examples/simple-js-env-vars/main.js
@@ -0,0 +1,10 @@
+export default async function main(event) {
+  const key = "MY_VAR";
+  const value = Azion.env.get(key);
+  if(!value) {
+    return new Response("Environment variable not found", { status: 500});
+  }
+  const response = `Hello Env Vars at ${value}!`;
+
+  return new Response(response, { status: 200});
+}
