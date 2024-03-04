5a0e769f38286d16ea4e7ec3c6eb8c9208fbb451
José Filho <jc.bsfilho@gmail.com>
2024-03-04 09:58:21 -0300

    docs: adding example of network list

---
+++ examples/simple-js-network-list/azion.config.js
@@ -0,0 +1,9 @@
+export default {
+  networkList: [
+    {
+      id: 1111,
+      listType: "ip_cidr",
+      listContent: ["127.0.0.0/8", "10.0.0.1"],
+    },
+  ],
+}
---
+++ examples/simple-js-network-list/vulcan.config.js
@@ -0,0 +1,8 @@
+export default {
+  builder: "esbuild",
+  entry: "main.js",
+  preset: {
+    name: "javascript",
+    mode: "compute",
+  },
+};
