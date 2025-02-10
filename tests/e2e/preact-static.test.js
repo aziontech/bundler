83fbd183a123f6f692644ef99b6a1e52cdba9f70
Pablo Diehl <pablodiehl@ymail.com>
2025-02-10 13:51:05 -0300

    feat: new preset for preact ssg (#433)

---
+++ tests/e2e/preact-static.test.js
@@ -0,0 +1,58 @@
+/* eslint-disable jest/expect-expect */
+import supertest from 'supertest';
+import puppeteer from 'puppeteer';
+import projectInitializer from '../utils/project-initializer.js';
+import projectStop from '../utils/project-stop.js';
+import { getContainerPort } from '../utils/docker-env-actions.js';
+
+// timeout in minutes
+const TIMEOUT = 10 * 60 * 1000;
+
+let serverPort;
+let localhostBaseUrl;
+const EXAMPLE_PATH = '/examples/preact-static';
+
+describe('E2E - preact-static project', () => {
+  let request;
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    serverPort = getContainerPort();
+    localhostBaseUrl = `http://0.0.0.0:${serverPort}`;
+
+    request = supertest(localhostBaseUrl);
+
+    await projectInitializer(EXAMPLE_PATH, 'react', serverPort);
+
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: 'new',
+    });
+    page = await browser.newPage();
+  }, TIMEOUT);
+
+  afterAll(async () => {
+    await projectStop(serverPort, EXAMPLE_PATH.replace('/examples/', ''));
+
+    await browser.close();
+  }, TIMEOUT);
+
+  test('Should render home page in "/" route', async () => {
+    await page.goto(`${localhostBaseUrl}/`);
+
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain('Get Started');
+    expect(pageContent).toContain('Learn Preact');
+    expect(pageTitle).toBe('Vite + Preact');
+  });
+
+  test('Should return correct asset', async () => {
+    await request
+      .get('/vite.svg')
+      .expect(200)
+      .expect('Content-Type', 'image/svg+xml');
+  });
+});
