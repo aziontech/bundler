ee7d388bbc761b2bb800f2032417504d5cd298c8
João Narciso <joaoznarciso@gmail.com>
2025-04-01 10:01:54 -0300

    fix: resolve CLEAN_OUTPUT_MODE env var

--- lib/commands/presets/command.ts
+++ lib/commands/presets/command.ts
@@ -1,5 +1,5 @@
 import { feedback } from 'azion/utils/node';
-import { getBeautify } from './presets';
+import { getBeautify, getKeys } from './presets';
 
 /**
  * @function
@@ -7,14 +7,20 @@ import { getBeautify } from './presets';
  * This command allows the user to create or list presets.
  * The user is guided by a series of prompts to enter a preset name.
  * @example
- *
  * // To list existing presets
  * presetsCommand('ls');
  */
 export async function presetsCommand(command: string) {
+  const isCleanOutputEnabled = process.env.CLEAN_OUTPUT_MODE === 'true';
+
   switch (command) {
     case 'ls':
-      getBeautify().forEach((preset: string) => feedback.option(preset));
+      if (isCleanOutputEnabled) {
+        getKeys().forEach((preset: string) => console.log(preset));
+      }
+      if (!isCleanOutputEnabled) {
+        getBeautify().forEach((preset: string) => feedback.option(preset));
+      }
       break;
 
     default:
