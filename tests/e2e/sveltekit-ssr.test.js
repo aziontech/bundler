1d57ee7c9e3fe0a0f7220f0cdc6757d3766968c4
pablodiehl <pablodiehl@ymail.com>
2026-01-27 09:35:38 -0300

    test(e2e): add tests for preset svelte

---
+++ tests/e2e/sveltekit-ssr.test.js
@@ -0,0 +1,71 @@
+import supertest from 'supertest';
+import puppeteer from 'puppeteer';
+import projectInitializer from '../utils/project-initializer.js';
+import projectStop from '../utils/project-stop.js';
+import { getContainerPort } from '../utils/docker-env-actions.js';
+
+// timeout in minutes
+const TIMEOUT = 10 * 60 * 1000;
+
+let serverPort;
+let localhostBaseUrl;
+const EXAMPLE_PATH = '/examples/sveltekit-commerce';
+
+describe('E2E - sveltekit-ssr project', () => {
+  let request;
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    serverPort = getContainerPort();
+    localhostBaseUrl = `http://0.0.0.0:${serverPort}`;
+
+    request = supertest(localhostBaseUrl);
+
+    await projectInitializer(EXAMPLE_PATH, 'svelte', serverPort);
+
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      headless: 'new',
+    });
+    page = await browser.newPage();
+  }, TIMEOUT);
+
+  afterAll(async () => {
+    await projectStop(serverPort, EXAMPLE_PATH.replace('/examples/', ''), EXAMPLE_PATH);
+
+    await browser.close();
+  }, TIMEOUT);
+
+  test('Should render home page in "/" route', async () => {
+    console.log("VASCO?")
+    await page.goto(`${localhostBaseUrl}/`, {
+      waitUntil: 'domcontentloaded',
+    });
+    
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain('Dessert dragée halvah croissant');
+    expect(pageTitle).toBe('Home – SvelteKit Commerce');
+  });
+
+  test('Should render a product page', async () => {
+    await page.goto(`${localhostBaseUrl}/product/acme-cup`, {
+      waitUntil: 'domcontentloaded',
+    });
+    
+    const pageContent = await page.content();
+    const pageTitle = await page.title();
+
+    expect(pageContent).toContain('Related Products');
+    expect(pageTitle).toBe('ACME Cup');
+  });
+
+  test('Should return correct asset', async () => {
+    await request
+      .get('/favicon.png')
+      .expect(200)
+      .expect('Content-Type', 'image/png');
+  });
+});
